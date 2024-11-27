bbb9629c7eef00c7490ede477ce5cc94b573765b
Maeen Bamatraf <mbamatra@lab1r2s8.42abudhabi.ae>
2024-11-27 14:50:56 +0400

    fixed atoi

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: mbamatra <mbamatra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/23 13:14:18 by mbamatra          #+#    #+#             */
-/*   Updated: 2024/01/24 20:02:44 by mbamatra         ###   ########.fr       */
+/*   Updated: 2024/11/27 14:17:13 by mbamatra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,26 @@ int	ft_atoi(const char *str)
 {
 	int					sign;
 	long long			result;
+	int					flag;
 
+	flag = 0;
 	result = 0;
 	sign = 1;
 	while (*str != '\0' && ((*str >= 9 && *str <= 13) || *str == 32))
 		str++;
 	if (*str == '-' || *str == '+')
 		if (*(str++) == '-')
-			sign *= -1;
+			sign = -1;
 	while (*str != '\0' && (*str >= '0' && *str <= '9'))
 	{
-		if (result > LLONG_MAX / 10 || (result == LLONG_MAX / 10
-				&& (*str - '0') > LLONG_MAX % 10))
-		{
-			if (sign == -1)
-				return (0);
-			else
-				return (-1);
-		}
+		flag = 1;
 		result = result * 10 + (*(str++) - '0');
+		if (result > 255 || (result > 0 && sign == -1))
+			return (-1);
 	}
+	while (*str && *str == ' ')
+		str++;
+	if (*str || !flag)
+		return (-1);
 	return (result * sign);
 }
